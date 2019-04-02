cf57031711c4958b3c5c32d383022f607dfa5044
Randall Morey <randall@randallmorey.com>
2019-04-02 16:22:34 -0400

    chore(babel): add async/await support

--- test/unit/smoke.js
+++ test/unit/smoke.js
@@ -2,8 +2,13 @@ import { assert } from 'chai';
 import server from '../../app/server';
 
 describe ('Unit: smoke test', () => {
-  it('should execute tests within a test module that imports an ES module from within the application', () => {
+  it('should execute tests with an imported ES module', () => {
     server();
     assert.ok(true, 'Tests should execute and this assertion should pass.');
+  });
+
+  it('should execute async tests', async () => {
+    const value = await Promise.resolve(3);
+    assert.equal(value, 3, 'Value was set with await.');
   });
 });
